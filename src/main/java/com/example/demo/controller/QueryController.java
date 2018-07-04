94d2e483d98ab768cefec85c0d705e50af1835e2
Cong-Yun <1045300328@qq.com>
2018-07-04 14:28:16 +0800

    添加query、details页面

---
+++ src/main/java/com/example/demo/controller/QueryController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Student;
+import com.example.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * @description
+ * @create 2018-07-01
+ **/
+@Controller
+public class QueryController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @RequestMapping(value = "/query",method = RequestMethod.GET)
+    public String login(HttpServletRequest request){
+        return "query";
+    }
+
+
+
+}
